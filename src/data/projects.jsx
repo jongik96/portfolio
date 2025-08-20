7216e310edb757c23bf338d24a926eb1276444c9
jongik96 <pji3504@naver.com>
2025-08-20 22:56:57 +0900

    update data

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,18 +1,18 @@
 export default [
   {
-    slug: "kansaiben-master",
-    title: "Kansaiben Master",
-    description: "標準語を関西弁に変換するサービス",
-    link: "https://kansaiben-converter.vercel.app/kansaiben",
-    thumbnail: "/img/ben.png",
-    tags: ["React", "OpenAI API", "TTS"],
+    slug: "pdfers",
+    title: "PDF File Format Converter",
+    description: "ファイル形式を変換するサービスサイト",
+    link: "https://pdfers.com",
+    thumbnail: "/img/pdferslogo.png",
+    tags: ["React", "Next.js", "Python"],
   },
   {
     slug: "payment-map",
     title: "Payment Map",
-    description: "교토 식당 결제수단(카드/모바일/현금) 공유 플랫폼.",
-    link: "https://your-project-link.com",
-    thumbnail: "/thumbs/paymentmap.png",
-    tags: ["React", "Google Maps API"],
+    description: "日本の飲食店決済方法共有プラットフォーム",
+    link: "https://paymap-japan.vercel.app/",
+    thumbnail: "/img/og-image-simple.svg",
+    tags: ["React","Next.js","Google Maps API","Redis"],
   },
 ];
